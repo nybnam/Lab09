6758b8bb73417cfa7e8063e62d3a599b720fbeed
User <User@LAPTOP-DE0OS0J2>
2018-04-11 21:35:41 +0900

    SalesReporter except Scanner

--- src/main/java/edu/handong/csee/java/Lab09/SalesReporter.java
+++ src/main/java/edu/handong/csee/java/Lab09/SalesReporter.java
@@ -8,15 +8,65 @@ public class SalesReporter {
 	private int numberOfAssociates;
 
 	public void getData(){
+		String name;
+		double sales
+		SalesAssociate associate = new SalesAssociate();
+		System.out.println("Enter the associate number" + numberOfAssociates);
+		System.out.println("Enter name of sales associate : ");
 		
+		associate.setName( );
+		System.out.println("Enter associate's sales : $");
+		associate.setSales( );
+		team[numberOfAssociates] = associate;
 	}
 	
 	public void computeStats(){
+		double SumforAverage = 0.0;
+		double Average;
+		double BestSales = 0.0;
+		String BestAssociate = "";
+		SalesAssociate associate_2 = new SalesAssociate();
+		for(int i = 0; i>= numberOfAssociates ;i++){
+			associate_2 = team[numberOfAssociates];
+			SumforAverage += associate_2.getSales();
+			
+			if(BestSales<=associate_2.getSales()){
+				BestSales=associate_2.getSales();
+				BestAssociate = associate_2.getName();
+			}
+		}
 		
+		Average = SumforAverage/numberOfAssociates;
+		associate_2.setName(BestAssociate);
+		associate_2.setSales(BestSales);
+		team[numberOfAssociates+1] = associate_2;
+		team[numberOfAssociates+2].setSales(Average);
 	}
 	
 	public void displayResults(){
+		SalesAssociate associate_3 = new SalesAssociate();
+		double deviation;
+		double Average = team[numberOfAssociates+2].getSales();
+		associate_3 = team[numberOfAssociates+1];
+		System.out.println("Average sales per associate is $" + Average);
+		System.out.println("The highest sales figure is $" + associate_3.getSales());
+		System.out.println("The following had the highest sales:");
+		System.out.println("Name : " + associate_3.getName());
+		System.out.println("Sales : $" + associate_3.getSales());
+		deviation = associate_3.getSales() - Average;
+		System.out.println("$" + deviation + "above the average.");
+		System.out.println("The rest performed as follows :\n\n");
 		
+		for(int i = 0;i>=numberOfAssociates;i++){
+			associate_3 = team[i];
+			System.out.println("Name : " + associate_3.getName());
+			System.out.println("Sales : $" + associate_3.getSales());
+			deviation = associate_3.getSales() - Average;
+			if (deviation>0)
+				System.out.println("$" + deviation + "above the average.\n");
+			else
+				System.out.println("$" + -deviation + "below the average.\n");
+		}
 	}
 	
 	public void run(){
