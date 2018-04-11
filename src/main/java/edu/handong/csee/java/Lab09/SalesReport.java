9c9d64d78dc6d3856e1f581791b34a190b82072d
User <User@LAPTOP-DE0OS0J2>
2018-04-11 15:40:06 +0900

    commit SalesReport

--- src/main/java/edu/handong/csee/java/Lab09/SalesReport.java
+++ src/main/java/edu/handong/csee/java/Lab09/SalesReport.java
@@ -1,7 +1,24 @@
 package edu.handong.csee.java.Lab09;
 
 public class SalesReport {
+	
+	private double highestSales;
+	private double averageSales;
+	private SalesAssociate[] team;
+	private int numberOfAssociates;
 
+	public void getDate(){
+		
+	}
+	
+	public void computeStats(){
+		
+	}
+	
+	public void displayResults(){
+		
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
