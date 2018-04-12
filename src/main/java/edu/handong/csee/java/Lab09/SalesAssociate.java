5894e1023a4eb13a9032163c92145eb88066098c
User <User@LAPTOP-DE0OS0J2>
2018-04-12 21:42:17 +0900

    complete SalesAssociate and SalesReporter

--- src/main/java/edu/handong/csee/java/Lab09/SalesAssociate.java
+++ src/main/java/edu/handong/csee/java/Lab09/SalesAssociate.java
@@ -10,27 +10,27 @@ package edu.handong.csee.java.Lab09;
  */
 
 public class SalesAssociate {
-	private String name;
-	private double sales;
+	private String name;   // associate's name
+	private double sales;  // associate's sales
 	
 	/** return name that SalesAssociate's member, name */
 	public String getName() {
-		return name;
+		return name; // return SaleAssociate's member name
 	}
 	
 	/** set name in SalesAssociate */
 	public void setName(String name) {
-		this.name = name;
+		this.name = name; // set name given string
 	}
 	
 	/** return sales that SalesAssociate's member, sales */
 	public double getSales() {
-		return sales;
+		return sales; // return SalesAssociate's member sales
 	}
 	
 	/** set sales in SalesAssociate */
 	public void setSales(double sales) {
-		this.sales = sales;
+		this.sales = sales; // set sales given double
 	}
 	
 }
--- src/main/java/edu/handong/csee/java/Lab09/SalesReporter.java
+++ src/main/java/edu/handong/csee/java/Lab09/SalesReporter.java
@@ -1,4 +1,6 @@
 package edu.handong.csee.java.Lab09;
+import java.util.Scanner;
+
 
 /**
  * This class calculate average sales, find highestSales, compare all associates to average, and display it.
@@ -9,62 +11,60 @@ package edu.handong.csee.java.Lab09;
  * 
  */
 
-
-import java.util.Scanner;
 public class SalesReporter {
 	
-	private double highestSales;
-	private double averageSales;
-	private SalesAssociate[] team;
-	private int numberOfAssociates=0;
+	private double highestSales;  // highest sales among the associates' sales
+	private double averageSales;  // average sales for the associates' sales
+	private SalesAssociate[] team;  // array that have SalesAssociate member
+	private int numberOfAssociates=0; // number of associates in array
 
 	/** Get name and sales from user and set them to SalesAssociate array team. */
 	public void getData(){
-		String name;
-		double sales;
-		Scanner keyboard = new Scanner(System.in);
-		SalesAssociate associate = new SalesAssociate();
-		System.out.println("Enter the associate number " + (numberOfAssociates+1));
-		System.out.println("Enter name of sales associate : ");
-		name = keyboard.nextLine();
-		System.out.println("Enter associate's sales : $");
-		sales = keyboard.nextDouble();
-		associate.setName(name);
-		associate.setSales(sales);
-		team[numberOfAssociates] = associate;
+		String name;		// declare variable that use for associate's name
+		double sales;		// declare variable that use for associate's sales
+		Scanner keyboard = new Scanner(System.in);  // declare scanner
+		SalesAssociate associate = new SalesAssociate();  // declare SalesAssociate class type instance
+		System.out.println("Enter the associate number " + (numberOfAssociates+1) +
+						   "\nEnter name of sales associate : ");  // request user to input name
+		name = keyboard.nextLine(); 	// get string from keyboard
+		System.out.println("Enter associate's sales : $");  //request user to input sales
+		sales = keyboard.nextDouble();	// get double from keyboard
+		associate.setName(name);	// set name to SalesAssociate class
+		associate.setSales(sales);	// set sales to SalesAssociate class
+		team[numberOfAssociates] = associate;	// set class to array(team)'s member
 	}
 	
 	/** find highest sales and calculate average sales */	
 	public void computeStats(){
-		double SumforAverage = 0.0;
-		highestSales = 0.0;
-		SalesAssociate associate_2 = new SalesAssociate();
+		double SumforAverage = 0.0; // declare variable that use for sales' sum 
+		highestSales = 0.0; // initialize highestSales to compare with other Sales
+		SalesAssociate associate_2 = new SalesAssociate(); // declare SalesAssociate class type instance
 		
 		for(int i = 0; i<=numberOfAssociates ;i++){
-			associate_2 = team[i];
-			SumforAverage += associate_2.getSales();
+			associate_2 = team[i];		// initialize associate_2 to team's member
+			SumforAverage += associate_2.getSales();  // make SumforAverage sales' sum
 			
 			if(highestSales<=associate_2.getSales()){
-				highestSales=associate_2.getSales();
+				highestSales=associate_2.getSales(); // find highestSales with comparing
 			}
 		}
 		
-		averageSales = SumforAverage/(numberOfAssociates+1);
+		averageSales = SumforAverage/(numberOfAssociates+1);  // divide SumforAverage to number of associate to get average
 		
 	}
 	
 	/** display highest sales and its associate, and the other associate and sales compare to average. */
 	public void displayResults(){
-		SalesAssociate associate_3 = new SalesAssociate();
+		SalesAssociate associate_3 = new SalesAssociate();  // declare SalesAssociate class type instance
 		
-		double deviation;
-		int bestAssociate = 0;
+		double deviation;	// declare variable for saving deviation compare to average
+		int bestAssociate = 0;  // declare variable to check where best sales is 
 		
 		for(int k = 0; k<=numberOfAssociates;k++){
 			if(team[k].getSales()==highestSales)
-				bestAssociate = k;
+				bestAssociate = k;	// save position of best sales at bestAssociate
 		}
-		associate_3 = team[bestAssociate];
+		associate_3 = team[bestAssociate]; // initialize associate_3 to team's member that have best sales 
 		
 		System.out.println( "\nAverage sales per associate is $" + averageSales +
 							"\nThe highest sales figure is $" + highestSales + "\n" +
@@ -72,45 +72,45 @@ public class SalesReporter {
 							"\nName : " + associate_3.getName() +
 							"\nSales : $" + highestSales +
 							"\n$" + (highestSales-averageSales) + " above the average.\n" +
-							"\nThe rest performed as follows :");
+							"\nThe rest performed as follows :"); // print associate's information that have best sales
 		
 		for(int i = 0;i<=numberOfAssociates;i++){
 			if(i==bestAssociate)
 				continue;
-			associate_3 = team[i];
-			System.out.println("Name : " + associate_3.getName());
-			System.out.println("Sales : $" + associate_3.getSales());
-			deviation = associate_3.getSales() - averageSales;
+			associate_3 = team[i];  // initialize associate_3 to team's member 
+			System.out.println("Name : " + associate_3.getName());  // print associate's name
+			System.out.println("Sales : $" + associate_3.getSales()); // print associate's sales
+			deviation = associate_3.getSales() - averageSales;	// initialize variable to substraction sales to averagesales   
 			if (deviation>=0)
-				System.out.println("$" + deviation + " above the average.\n");
+				System.out.println("$" + deviation + " above the average.\n"); // print if sales is more than average
 			else
-				System.out.println("$" + (-deviation) + " below the average.\n");
+				System.out.println("$" + (-deviation) + " below the average.\n");  // print if sales is less than average
 		}
 	}
 	
 	/** run methods that in SalesReporter */
 	public void run(){
-		int n;
-		System.out.println("Enter number of sales associates");
-		Scanner scanner = new Scanner(System.in);
-		n = scanner.nextInt();
-		SalesAssociate[] A = new SalesAssociate[10];
-		team = A;
+		int n; // declare int type variable that use for input
+		System.out.println("Enter number of sales associates"); // request number of sales associates 
+		Scanner scanner = new Scanner(System.in); // declare scanner
+		n = scanner.nextInt(); // get int from keyboard
+		SalesAssociate[] A = new SalesAssociate[10]; // make array that consist of SalesAssoicate
+		team = A; // make team array that consist of SalesAssoicate
 		
 		for(int t=0;t<n;t++){
-			numberOfAssociates = t;
-			getData();
+			numberOfAssociates = t; // initialize variable to t
+			getData(); // run getData()
 		}
 		
-		computeStats();
-		displayResults();
+		computeStats(); // run computeStats()
+		displayResults(); // run displayResults() 
 	}
 
 	/** Do run() method */
 	public static void main(String[] args) {
 		
-		SalesReporter reporter = new SalesReporter();
-		reporter.run();
+		SalesReporter reporter = new SalesReporter(); // declare SalesAssociate class type instance
+		reporter.run(); // run run()
 	}
 
 }
